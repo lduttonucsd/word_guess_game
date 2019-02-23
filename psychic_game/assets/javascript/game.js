f3ce3f7582558e0cb95f2d0fd42c879871ad15dc
lduttonucsd <47542535+lduttonucsd@users.noreply.github.com>
2019-02-22 23:58:30 -0800

    Add files via upload
    something went wrong with my github repo and i cant find it now :[ figured i could at least get this in before deadline

---
+++ psychic_game/assets/javascript/game.js
@@ -0,0 +1,25 @@
+// display choices app can make
+var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+// app chooses random letter
+
+
+// user types guess
+
+
+
+// display user guess
+document.onkeydown = function (event) {
+    var userSelection = event.key;
+    userChoice.textContent = userSelection
+
+// compare user guess to app choice
+
+// display correct or incorrect
+
+// countdown of remaining guesses
+
+// display wins
+
+// display losses 
